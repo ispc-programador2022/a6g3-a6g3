3bb841b51bccb9cb724c105a1c31ce272436aeaa
Gustavo Godoy <gustavogodoy85@gmail.com>
2022-07-11 19:59:17 -0300

    Agregado de la funcion producto

--- bloque1/f_producto.py
+++ bloque1/f_producto.py
@@ -5,3 +5,10 @@
 Recibe 2 valores enteros.
 Retorna el producto de los 2 elementos.
 '''
+
+def producto(a,b):
+    if type(a) == int and type(b) == int:
+        return a*b
+    else:
+        return('Los valores ingresados son incorrectos.')
+
