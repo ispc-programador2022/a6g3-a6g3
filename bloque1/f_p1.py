b287c232bcea3a0a25ddff94693070019c5df746
Gustavo Godoy <gustavogodoy85@gmail.com>
2022-07-11 20:02:54 -0300

    Agregado de la funcion p1

--- bloque1/f_p1.py
+++ bloque1/f_p1.py
@@ -10,3 +10,20 @@ funciones anteriores.
 Retorna la resta de los 2 primero por el 3er parámetros, usando las funciones
 anteriores
 '''
+from f_producto import producto
+from f_suma import suma
+from f_resta import resta
+
+def p1(a,b,c):
+    if type(a)==int and type(b)==int and type(c)==int:      #compruebo el tipo de valores ingresados
+        #res = (suma(producto(a,b),c),producto(suma(a,b),c),producto(resta(a,b),c))
+        res_1 = suma(producto(a,b),c)
+        res_2 = producto(suma(a,b),c)
+        res_3 = producto(resta(a,b),c)
+        res = (res_1,res_2,res_3)
+        return res
+    else:
+        return('Los valores ingresados son incorrectos.')
+
+#Prueba (no puede realizarse si las funciones f_suma, f_resta y f_producto aun no fueron escritas)
+#p1(1,5,7)
