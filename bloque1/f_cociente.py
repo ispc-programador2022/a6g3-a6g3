fb1e575d64b719e4593ee3bfdfda20c9e9fc3518
Nicolás González Da Silva <klasgodasi@gmail.com>
2022-07-12 18:29:24 -0300

    Carga de función cociente

--- bloque1/f_cociente.py
+++ bloque1/f_cociente.py
@@ -5,3 +5,13 @@
 Recibe 2 valores enteros.
 Retorna el cociente de los 2 elementos.
 '''
+
+
+def cociente(numerador,denominador):
+    if type(numerador) == int and type(denominador) == int:
+        if denominador != 0:
+            return numerador/denominador
+        else:
+            return("El denominador no puede ser cero.")
+    else:
+        return("Los números ingresados no son enteros.")
