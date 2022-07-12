de608d75d3dfc1d69cb2c31033b03e83eca7a372
GuidoneDamian <damianguidone@gmail.com>
2022-07-12 04:56:01 -0300

    Funciones para <probar>

--- bloque1/f_potencia.py
+++ bloque1/f_potencia.py
@@ -5,3 +5,12 @@
 Recibe 2 valores enteros.
 Retorna la potencia del primero elevado al segundo parámetros.
 '''
+def potencia(a,b):       
+    if type(a) == int and type(b) == int:
+        return a**b
+    else:
+        print('Los valores ingresados no son correctos.\nInténtelo nuevamente.')
+        pass
+
+#Probar
+#potencia(2,3)
--- bloque1/f_radicacion.py
+++ bloque1/f_radicacion.py
@@ -5,3 +5,12 @@
 Recibe 2 valores enteros.
 Retorna la raiz del primero respecto del segundo parámetros.
 '''
+def radicacion(a,b):
+    if type(a) == int and type(b) == int:
+        return b**(1/a)
+    else:
+        print('Los valores ingresados no son correctos.\nInténtelo nuevamente.')
+        pass
+
+#Probar
+#radicacion(2,3)
