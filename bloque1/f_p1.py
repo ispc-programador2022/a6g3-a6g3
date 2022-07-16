a896ad4217ae1d70663790fd80a76eddbfec5156
chulke <gusavogodoy85@gmail.com>
2022-07-15 22:14:22 -0300

    Fix en imports y control de errores

--- bloque1/f_p1.py
+++ bloque1/f_p1.py
@@ -16,13 +16,23 @@ import bloque1.f_suma as suma
 import bloque1.f_resta as resta
 
 
-def p1(a,b,c):
+
+
+def p1(a,b):
+    cont = True
+    while cont:
+        try:
+            c = int(input('Ingrese un tercer valor del tipo entero: '))
+            cont = False
+        except ValueError:
+            print('El valor ingresado no es un entero. Intente nuevamente o presione "ctrl+q".')
+            cont = True
     if type(a)==int and type(b)==int and type(c)==int:      #compruebo el tipo de valores ingresados
         #res = (suma(producto(a,b),c),producto(suma(a,b),c),producto(resta(a,b),c))
         res_1 = suma.suma(producto.producto(a,b),c)
         res_2 = producto.producto(suma.suma(a,b),c)
         res_3 = producto.producto(resta.resta(a,b),c)
-        res = (res_1,res_2,res_3)
+        res = (res_1,res_2,res_3,c)
         return res
     else:
         return('Los valores ingresados son incorrectos.')
