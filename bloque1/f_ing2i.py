81dc653157a518d59a06c0bbe6a5f41f4452a953
chulke <gusavogodoy85@gmail.com>
2022-07-15 22:12:54 -0300

    Fix para evitar salidas del programa

--- bloque1/f_ing2i.py
+++ bloque1/f_ing2i.py
@@ -5,17 +5,21 @@
 Pide el ingreso por teclado de 2 enteros.
 Retorna los enteros en una lista de 2 elementos.
 '''
+
 def ing2i():
-    print("Funcion ing2i:")
-    try:
-        a=int(input("Ingrese el primer entero: "))
-        b=int(input("Ingrese el segundo entero: "))
-    except TypeError:
-        print('TypeError: Falló la función, inténtelo nuevamente')
-        return
-    except ValueError:
-        print('ValueError: Falló la función, inténtelo nuevamente')
-        return 
+    #print("Funcion ing2i:")
+    cont = True
+    while cont:
+        try:
+            a=int(input("Ingrese el primer entero: "))
+            b=int(input("Ingrese el segundo entero: "))
+            cont = False
+        except TypeError:
+            print('TypeError: Falló la función, inténtelo nuevamente')
+            #return
+        except ValueError:
+            print('ValueError: Falló la función, inténtelo nuevamente')
+            #return 
     return [a,b]
 
 #Prueba:
