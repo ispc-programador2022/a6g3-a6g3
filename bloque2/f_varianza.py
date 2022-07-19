4d3d485e16aa7a98e0d0c2ee29bd28e6acad5cfe
Gustavo Godoy <gustavogodoy85@gmail.com>
2022-07-18 23:12:52 -0300

    Carga de función varianza

--- bloque2/f_varianza.py
+++ bloque2/f_varianza.py
@@ -3,4 +3,27 @@
 
 '''
 Función que calcule la varianza del vector obtenido en genrnd.
+
+La Varianza es una medida de dispersión que se utiliza para representar 
+la variabilidad de un conjunto de datos respecto de la media aritmética 
+de los mismo. Así, se calcula como la suma de los residuos 
+elevados al cuadrado y divididos entre el total de observaciones.
 '''
+
+
+
+from f_media import media
+
+
+def varianza(vector):
+    med = media(vector)
+    l_restos = []
+    for i in vector:               
+        nume = (i - med)**2
+        l_restos.append(nume)       #armo una lista con las diferencias entre los valores del vector y la media, elevadas al cuadrado
+    total_restos = sum(l_restos)
+    return total_restos/len(vector) #(total_restos/len(vector))**(1/2) desviación típica
+
+#Test
+#from f_genrnd import genrnd
+#print(varianza(genrnd()))
