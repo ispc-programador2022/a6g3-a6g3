a4fb6f949f10098c7438af9213272f1773fb5fc2
Gustavo Godoy <gustavogodoy85@gmail.com>
2022-07-18 23:12:14 -0300

    Carga de función media

--- bloque2/f_media.py
+++ bloque2/f_media.py
@@ -3,4 +3,21 @@
 
 '''
 Función que calcule la media del vector obtenido en genrnd.
+
+Calculo la media aritmética (por defecto, ya que no se especifica
+que media se debe calcular)
 '''
+
+
+def media(vector):
+    res = 0
+    for i in vector:
+        res = res + i
+    med = res/len(vector)
+    return med
+
+#Test
+# import random
+# import f_genrnd
+# lista = f_genrnd.genrnd()
+# print(media(lista))
