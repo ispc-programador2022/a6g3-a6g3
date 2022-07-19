4172e320417d09d7084a147602beb2cf46e30e01
Gustavo Godoy <gustavogodoy85@gmail.com>
2022-07-18 21:42:36 -0300

    Carga funcion genrnd

--- bloque2/f_genrnd.py
+++ bloque2/f_genrnd.py
@@ -4,3 +4,14 @@
 '''
 función genrnd que retorna una lista con 50 números aleatorios.
 '''
+
+import random
+
+def genrnd():
+    lista_rnd = []
+    for i in range(50):
+        lista_rnd.append(random.randint(0,1000))
+    return lista_rnd
+
+#Test
+#print(genrnd())
