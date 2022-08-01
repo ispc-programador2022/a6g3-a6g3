11ca5ab8d2cd3f6f192c22a8b56f9f4c76dca0e2
GuidoneDamian <damianguidone@gmail.com>
2022-08-01 05:20:41 -0300

    Genero 5*10**17

--- bloque3/f_genrnd_.py
+++ bloque3/f_genrnd_.py
@@ -1,1 +1,24 @@
 
+
+""" Retorna una lista con 500.000.000.000.000.000 números aleatorios. 
+
+5*10^17
+"""
+
+import random
+
+def barra_progreso(progreso, total):
+    porcentage=100*(progreso/float(total))
+    barra=('|'*int(porcentage) + '-'*(100-int(porcentage)))
+    print(f"\r|{barra}| {porcentage:.2f}%",end="\r")
+
+def genrnd():
+    print("Calculando nùmeros 500.000.000.000.000.000 aleatorios:")
+    lista_rnd = []
+    for i in range(5*10**17):
+        lista_rnd.append(random.randint(0,1000))
+        barra_progreso(i+1, 5*10**17)
+    return lista_rnd
+
+#Test
+#print(genrnd())
