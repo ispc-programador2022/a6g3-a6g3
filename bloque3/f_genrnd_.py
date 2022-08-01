36f924a585d29ae0b58d0b33bc7c224080d14694
GuidoneDamian <damianguidone@gmail.com>
2022-08-01 06:15:01 -0300

    Ahorro en tiempos de carga

--- bloque3/f_genrnd_.py
+++ bloque3/f_genrnd_.py
@@ -17,8 +17,9 @@ def genrnd():
     lista_rnd = []
     for i in range(5*10**17):
         lista_rnd.append(random.randint(0,1000))
-        barra_progreso(i+1, 5*10**17)
+        if i%(5*10**13)==0:
+            barra_progreso(i+1, 5*10**17)
     return lista_rnd
 
 #Test
-#print(genrnd())
+print(genrnd())
